c8bc9cba2a1e1c619ad56d27e43a514511c2a4bc
Peter Washington <pyw1@rice.edu>
2014-02-09 14:55:59 -0600

    Made changes to fix the sample voice app.

--- SpeechKit-Android-1.4.7/SampleVoiceApp/gen/com/nuance/nmdp/sample/R.java
+++ SpeechKit-Android-1.4.7/SampleVoiceApp/gen/com/nuance/nmdp/sample/R.java
@@ -48,20 +48,20 @@ public final class R {
         public static final int beep=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
+        public static final int app_name=0x7f05000b;
         public static final int button_dictation=0x7f05000d;
-        public static final int button_startDictation=0x7f050009;
-        public static final int button_startTts=0x7f050008;
-        public static final int button_stop=0x7f050007;
+        public static final int button_startDictation=0x7f050007;
+        public static final int button_startTts=0x7f050006;
+        public static final int button_stop=0x7f050005;
         public static final int button_tts=0x7f05000e;
-        public static final int button_websearch=0x7f050002;
-        public static final int hello=0x7f050000;
-        public static final int radio_endPointerLong=0x7f050005;
-        public static final int radio_endPointerNone=0x7f050006;
-        public static final int radio_endPointerShort=0x7f050004;
-        public static final int text_endPointer=0x7f050003;
-        public static final int title_dictation=0x7f05000a;
+        public static final int button_websearch=0x7f050000;
+        public static final int hello=0x7f05000a;
+        public static final int radio_endPointerLong=0x7f050003;
+        public static final int radio_endPointerNone=0x7f050004;
+        public static final int radio_endPointerShort=0x7f050002;
+        public static final int text_endPointer=0x7f050001;
+        public static final int title_dictation=0x7f050008;
         public static final int title_main=0x7f05000c;
-        public static final int title_tts=0x7f05000b;
+        public static final int title_tts=0x7f050009;
     }
 }
--- SpeechKit-Android-1.4.7/SampleVoiceApp/src/com/nuance/nmdp/sample/AppInfo.java
+++ SpeechKit-Android-1.4.7/SampleVoiceApp/src/com/nuance/nmdp/sample/AppInfo.java
@@ -27,17 +27,13 @@ public class AppInfo
      */
 
     /* Please contact Nuance to receive the necessary connection and login parameters */
-    public static final String SpeechKitServer = /* Enter your server here */;
+    public static final String SpeechKitServer = "bgm.nmdp.nuancemobility.net";
 
-    public static final int SpeechKitPort = /* Enter your port here */;
+    public static final int SpeechKitPort = 443;
     
     public static final boolean SpeechKitSsl = false;
 
-    public static final String SpeechKitAppId = /* Enter your ID here */;
-
-    public static final byte[] SpeechKitApplicationKey = {
-        /* Enter your application key here:
-        (byte)0x00, (byte)0x01, ... (byte)0x00
-        */
-    };
+    public static final String SpeechKitAppId = "NMDPPRODUCTION_Rice_University_SPeach_20140208201915";
+    
+ 	public static final byte[] SpeechKitApplicationKey = {(byte)0xa8, (byte)0x72, (byte)0x87, (byte)0xf2, (byte)0x68, (byte)0xe8, (byte)0xd5, (byte)0x62, (byte)0x69, (byte)0xf0, (byte)0xee, (byte)0x17, (byte)0x71, (byte)0x2f, (byte)0xd5, (byte)0x4e, (byte)0x21, (byte)0x66, (byte)0x04, (byte)0x33, (byte)0xcc, (byte)0x77, (byte)0xe9, (byte)0xd3, (byte)0xd6, (byte)0xa6, (byte)0xa9, (byte)0x0c, (byte)0x8e, (byte)0x59, (byte)0x20, (byte)0x36, (byte)0xcb, (byte)0xa8, (byte)0x29, (byte)0x34, (byte)0x90, (byte)0xfe, (byte)0x96, (byte)0xf9, (byte)0xef, (byte)0x84, (byte)0x10, (byte)0x60, (byte)0xb3, (byte)0xe5, (byte)0xaf, (byte)0xd5, (byte)0x4b, (byte)0xb3, (byte)0x08, (byte)0x18, (byte)0x2b, (byte)0xf1, (byte)0xeb, (byte)0x01, (byte)0x97, (byte)0x95, (byte)0x0d, (byte)0x26, (byte)0xee, (byte)0x2d, (byte)0x5c, (byte)0xba};
 }
