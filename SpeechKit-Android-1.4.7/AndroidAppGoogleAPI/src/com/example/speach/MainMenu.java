3da6866dc21e4e636658afd5e35be6a577afa3b6
aaronroe <aaronalkan@gmail.com>
2014-04-21 14:56:09 -0500

    Changes app logo

--- SpeechKit-Android-1.4.7/AndroidAppGoogleAPI/src/com/example/speach/MainMenu.java
+++ SpeechKit-Android-1.4.7/AndroidAppGoogleAPI/src/com/example/speach/MainMenu.java
@@ -1,10 +1,24 @@
 package com.example.speach;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * This is the first activity that loads on boot.
@@ -34,8 +48,38 @@ public class MainMenu extends Activity {
      * @param view
      */
     public void startMedicalMode(View view) {
-    	Intent intent = new Intent(this, MedicalMode.class);
-    	startActivity(intent);
+		EditText sessionNameInput = (EditText) findViewById(R.id.editText1);
+		final String sessionName = sessionNameInput.getText().toString();
+		
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				// get the session id for the new session from the server.
+			    HttpClient httpclient = new DefaultHttpClient();
+			    HttpPost httppost = new HttpPost("http://www.yoursite.com/script.php");
+
+			    try {
+			        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+			        nameValuePairs.add(new BasicNameValuePair("name", sessionName));
+
+			        // Execute HTTP Post Request
+			        HttpResponse response = httpclient.execute(httppost);
+			        
+			    } catch (ClientProtocolException e) {
+			        // TODO Auto-generated catch block
+			    } catch (IOException e) {
+			        // TODO Auto-generated catch block
+			    }
+		    	
+			    // start the medical mode.
+		    	Intent intent = new Intent(MainMenu.this, MedicalMode.class);
+		    	startActivity(intent);	
+			}
+		};
+		
+		Thread t = new Thread();
+		
+		t.start();
     }
     
 }
